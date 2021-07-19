9029d44f61f7c8877a4bdf0997ab794b1abc0c8f
Rojar Smith <rojarsmith@gmail.com>
2021-07-19 14:06:40 +0800

    Integrate spring cloud gateway and actuator.

---
+++ ocean-gateway/src/main/java/ocean/gateway/config/DiscoveryConfig.java
@@ -0,0 +1,14 @@
+package ocean.gateway.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Rojar Smith
+ *
+ * @date 2021-07-19
+ */
+@EnableDiscoveryClient
+@Configuration
+public class DiscoveryConfig {
+}
